c784bc5291ea8090aff895fc0819b6f1cdd832e9
AppWeather2021 <71829789+AppWeather2021@users.noreply.github.com>
2020-10-29 11:13:36 +0500

    Add files via upload

---
+++ telegram.php
@@ -0,0 +1,28 @@
+<?php
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(0);
+
+$name = $_POST['user_name'];
+$email = $_POST['user_email'];
+$message = $_POST['user_message'];
+$token = "1386113680:AAFUGu4cbH1XYdm9rI6wBtt0B7YJMScXgD0";
+$chat_id = "-1001349917089";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Email:' => $email,
+  'Сообщение: ' => $message
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Очень много запросов! Перейдите на главную, повторите отправку через 5s";
+}
+?>
